fd5b81b69d94b5b4a42f217137bcfb7624ea2131
NgPhuc <nguyentrongphuc25102005@gmail.com>
2026-03-11 21:23:35 +0700

    Delete phuc directory

--- phuc/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db = 'db_ctu_comic';
-$conn = new mysqli($host, $user, $pass, $db);
-if ($conn->connect_error) 
-    die('Kết nối thất bại: ' . $conn->connect_error);
-?>
